8ee70ebf2fb967414568b69472fff6e7642089f2
Li Chun Hua <jim870718@gmail.com>
2023-01-23 17:20:37 +0800

    belbin-form v0.1

--- belbin-form/script/script.js
+++ belbin-form/script/script.js
@@ -43,5 +43,99 @@ for (var si = 1; si <= 7; si++) {
 }
 
 checkForm = () => {
-    return false
+    var rtn_value = true
+    var page_offsetX = null
+
+    // check name
+    var name_object = document.getElementById('name')
+    if (!name_object.value.trim()) {
+        rtn_value = false
+
+        name_object.parentElement.classList.add('warning')
+        name_object.focus()
+
+        page_offsetX = page_offsetX == null ? name_object.parentElement.offsetTop : page_offsetX
+    } else {
+        name_object.parentElement.classList.remove('warning')
+    }
+
+    // check id
+    var id_object = document.getElementById('id')
+    if (!id_object.value.trim()) {
+        rtn_value = false
+
+        id_object.parentElement.classList.add('warning')
+        id_object.focus()
+
+        page_offsetX = page_offsetX == null ? id_object.parentElement.offsetTop : page_offsetX
+    } else {
+        id_object.parentElement.classList.remove('warning')
+    }
+
+    // check department
+    var department_value = null
+    for (var i = 0; i < 6; i++) {
+        var department_object = document.getElementById(`department-${i}`)
+        if (department_object.checked) {
+            department_value = department_object.value
+            break
+        }
+    }
+    if (document.getElementById('department-others-radio').checked) {
+        department_value = document.getElementById('department-others').value
+    }
+    if (!department_value) {
+        rtn_value = false
+
+        document.getElementById('department-others-radio').parentElement.parentElement.classList.add('warning')
+
+        page_offsetX = page_offsetX == null ? document.getElementById('department-others-radio').parentElement.parentElement.offsetTop : page_offsetX
+    } else {
+        document.getElementById('department-others-radio').parentElement.parentElement.classList.remove('warning')
+    }
+
+    // check group
+    var group_object = document.getElementById('group')
+    if (!group_object.value.trim()) {
+        rtn_value = false
+
+        group_object.parentElement.classList.add('warning')
+        group_object.focus()
+
+        page_offsetX = page_offsetX == null ? group_object.parentElement.offsetTop : page_offsetX
+    } else {
+        group_object.parentElement.classList.remove('warning')
+    }
+
+    var output_strs = []
+    var number_ch_list = ['ㄧ', '二', '三', '四', '五', '六', '七']
+
+    for (var si = 0; si < 7; si++) {
+        if (remaining_values[si] != 0) {
+            rtn_value = false
+
+            output_strs.push(`第${number_ch_list[si]}大題尚餘 ${remaining_values[si]} 分`)
+            document.getElementsByClassName('section')[si + 2].classList.add('warning')
+
+            page_offsetX = page_offsetX == null ? document.getElementsByClassName('section')[si + 2].offsetTop : page_offsetX
+        } else {
+            document.getElementsByClassName('section')[si + 2].classList.remove('warning')
+        }
+    }
+    if (output_strs) {
+        alert(output_strs.join('\n'))
+    }
+
+    // return value
+
+    if (!rtn_value) {
+        window.scrollTo({ top: page_offsetX - 30, behavior: 'smooth' })
+    }
+
+    return rtn_value
 }
+
+//
+document.getElementById('department-others').addEventListener('focusout', (e) => {
+    document.getElementById('department-others-radio').value = e.target.value
+})
