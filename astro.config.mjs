d10879708f419e1379f64ce91ac654d41f400c28
chunhua <chunhua.2147483647@gmail.com>
2026-09-12 18:09:44 +0800

    feat: add tailwind css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://tibittw.github.io",
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
